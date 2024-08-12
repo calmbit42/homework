83f1cf7604b3f79faba5665d0162cb339a0f6513
calmbit42 <calmbit@gmail.com>
2024-08-12 16:56:29 +0300

    add homework6

---
+++ js_homework_9.1.js
@@ -0,0 +1,59 @@
+const company = {
+    name: 'Велика Компанія',
+    type:'Головна компанія',
+    platform: 'Платформа для продажу квитків',
+    sellsSolution: 'Рішення для продажу квитків',
+    clients: [
+      {
+        name: 'Клієнт 1',
+        type: 'subCompany',
+        uses: 'ПО для продажу квитків',
+        sells: 'Рішення для продажу квитків',
+        partners: [
+          {
+            name: 'Клієнт 1.1',
+            type: 'subSubCompany',
+            uses: 'Рішення для продажу квитків',
+            sells: 'Рішення для продажу квитків',
+          },
+          {
+            name: 'Клієнт 1.2',
+            type: 'subSubCompany',
+            uses: 'Рішення для продажу квитків',
+            sells: 'Рішення для продажу квитків',
+            partners: [
+              {
+                name: 'Клієнт 1.2.3',
+                type: 'subSubCompany',
+                uses: 'Рішення для продажу квитків',
+                sells: 'Рішення для продажу квитків',
+              }
+            ]
+          }
+        ]
+      },
+      {
+        name: 'Клієнт 2',
+        type: 'subCompany',
+        uses: 'ПО для продажу квитків',
+        sells: 'Рішення для продажу квитків'
+      }
+    ],
+    findValueByKey: function(companyName) {
+        let clients = this.clients.map((x) => x);
+
+        while (clients.length) {
+            let client = clients.shift();
+
+            if (client.name === companyName) {
+                return client;
+            }
+
+            if (client.partners) {
+                clients = client.partners.concat(clients);
+            }
+        }
+
+        return null;
+    }
+  };
